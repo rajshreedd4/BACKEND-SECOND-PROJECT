e124f7ea4fd8ef9d38b606a14cc140b00eb8bfbe
Rajshree Dangat <rajshreedd4@gmail.com>
2026-06-24 13:59:40 +0530

    Updated controllers/user.js

--- controllers/user.js
+++ controllers/user.js
@@ -153,8 +153,11 @@ exports.logout = async (req, res) => {
             message: "Logout successful"
         });
     } catch (error) {
-        res.status(500).json({
-            message: "Internal server error"
-        });
-    }
+    console.error(error);
+
+    res.status(500).json({
+        message: "Internal server error",
+        error: error.message
+    });
+}
 };
